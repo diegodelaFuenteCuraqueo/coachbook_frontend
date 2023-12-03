e820d203959c743887ba94a4c16186f3f1b54307
diegodelaFuenteCuraqueo <diego.delafuente@ug.uchile.cl>
2023-12-02 21:08:15 -0300

    fix host

--- src/constants.js
+++ src/constants.js
@@ -16,7 +16,7 @@ export const URL = {
     //cancelBooking: '/api/cancel-booking',
     //editBooking: '/api/edit-booking',
   },
-  LOCALHOST: 'http://192.168.101.8:5000', // "http://localhost:5000"
+  LOCALHOST: "http://localhost:5000", // "http://192.168.101.8:5000"
 
 
 }
